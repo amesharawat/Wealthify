09ffc891ec24bb6b20b51f5f6fb9c49998fd7392
amesharawat <160380500+amesharawat@users.noreply.github.com>
2026-06-18 21:21:25 +0530

    Preparing for deployment

---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from 'sonner'
+
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Wealthify",
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className}`}
+        >
+          {/* header */}
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+};
